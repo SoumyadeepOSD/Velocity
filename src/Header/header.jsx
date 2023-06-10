96b8b3e610c36f6592b2c6fc104fb14457d1796e
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-10 14:05:11 +0530

    Header building: phase-1[done]

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,11 +1,54 @@
-import '../index.css'
+import '../index.css';
+import {useState, useEffect} from 'react';
+import { AiOutlineAlignLeft, AiOutlineClose } from 'react-icons/ai';
+
+const useWindowSize = () => {
+  const [ size, setSize ] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(()=>{
+    const handleResize = () => {
+      setSize([window.innerHeight, window.innerWidth]);
+    };
+    window.addEventListener("resize", handleResize);
+  },[]);
+  return size;
+}
+
 
 const Header = () =>
 {
+  const [istap, setTap] = useState(false);
+  const [height, width] = useWindowSize();
+  const Tap = ()=>{
+  setTap(!istap);
+  console.log(istap);
+}
+
   return (
-    <div className='header margin'>
-    </div>
+      <div className='menu'>
+        <div className='logo'>
+          <img src="src/assets/velocity_fire.png" className='fire' alt="" />
+          <p className='velocity'>Velocity.io</p>
+        </div>
+          {width >= 500 ? 
+          (<ul style={Style.menuStyle} className='menu-items-horizontal mobile-menubar'>
+          <li><a className='tab' href="">Services</a></li>
+          <li><a className='tab' href="">Tools</a></li>
+          <li><a className='tab' href="#about-container">About</a></li>
+          <li><a className='tab' href="#contact-container">Contact</a></li>
+          <li>{width}</li>
+        </ul>):
+        istap ?
+        (<AiOutlineClose size={30} onClick={Tap}/>):
+        (<AiOutlineAlignLeft size={30} onClick={Tap}/>)}
+       {istap ? <p style={{position:'absolute', background:'red',padding:'40%', top:50}}>Hello</p>:null}
+      </div>
   )
 }
 
 export default Header;
+
+const Style = {
+  menuStyle:{
+
+  }
+};
