c7cbe10d2ab28014e545869cdea1cf15f819e5f7
SoumyadeepOSD <opencontainer255@gmail.com>
2023-06-10 14:07:10 +0530

    Minor bug fixed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,6 @@
 function About() {
   return (
-    <div className="about-container">
+    <div className="about-container" id="about-container">
         <p className="black">About</p>
         <span className="rainbow-bar"></span>
         <p className="description">Developers always seek for getting everything at a single place to do their all development tasks regularly. Velocity.io allow us to access all kind of tools, components, APIs at a single place. You can focus on building, rest of all will be taken care by us</p>
